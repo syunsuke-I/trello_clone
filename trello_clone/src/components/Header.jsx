207dc027506e14126c48417fc293b6033b779905
syunsuke-I <odol@ymail.ne.jp>
2023-11-19 19:55:41 +0900

    ファーストコミット

---
+++ trello_clone/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export const Header = () =>{
+  return(
+    <>
+      <div>
+        <header>
+          <h1>Simple Trello</h1>
+        </header>
+      </div>
+    </>
+  )
+}
+
+export default Header;
